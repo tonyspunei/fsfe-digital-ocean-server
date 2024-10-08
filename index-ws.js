b866f285e3838e3b20449dca1dae3b1ee28a2e96
tonyspunei <tony.spunei@thearc.de>
2024-10-08 04:25:05 +0200

    add database loggging

--- index-ws.js
+++ index-ws.js
@@ -12,6 +12,15 @@ server.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
 
+process.on("SIGINT", function() {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(function() {
+    shutdownDB();
+  });
+});
+
 /* Begin Websocket */
 const WebSocket = require("ws").Server;
 const wss = new WebSocket({server: server});
@@ -25,6 +34,16 @@ wss.on("connection", function connection(ws) {
   if(ws.readyState === ws.OPEN) {
     ws.send("Welcome to my server!");
   }
+
+  db.run(
+    `INSERT INTO visitors (count, time) VALUES (?, datetime('now'))`,
+    [numClients],
+    function(err) {
+      if (err) {
+        console.error('Error inserting visitor count:', err);
+      }
+    }
+  );
   
   ws.on("close", function close() {
     wss.broadcast("Current visitors: " + numClients);
@@ -39,3 +58,29 @@ wss.broadcast = function broadcast(data) {
     }
   });
 };
+
+/* End Websocket */
+
+/* Begin Database SQLite */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(function() {
+  db.run("CREATE TABLE visitors (count INTEGER, time TEXT)");
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", function(err, row) {
+    if (err) {
+      console.error("Error fetching counts:", err);
+    } else {
+      console.log(row);
+    }
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Database shutting down...");
+  db.close();
+}
